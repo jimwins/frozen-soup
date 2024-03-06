c7bb3618ac4209fc035ced5ed9c4348162afd836
jim winstead <jimw@trainedmonkey.com>
2024-03-06 13:34:38 -0800

    Add first API entry point (freeze_to_string) and test
    Doesn't actually do any of the 'freezing' stuff yet, but just laying some
    groundwork here.

--- frozen_soup/__init__.py
+++ frozen_soup/__init__.py
@@ -1,2 +1,10 @@
-def example_function():
-    return 1 + 1
+from typing import Optional
+
+import requests
+
+def freeze_to_string(url: str, session: Optional[requests.Session]) -> str:
+    if session is None:
+        session = requests.Session()
+
+    r = session.get(url)
+    return r.text
--- tests/test_frozen_soup.py
+++ tests/test_frozen_soup.py
@@ -1,5 +1,17 @@
-from frozen_soup import example_function
+import pytest
+import requests
+from requests_testadapter import TestAdapter, TestSession
+
+from frozen_soup import freeze_to_string
+
+@pytest.fixture
+def session() -> requests.Session:
+    s = TestSession()
+    s.mount('http://test/simple-string', TestAdapter(b'Mock!'))
+    return s
 
 
-def test_example_function():
-    assert example_function() == 2
+def test_freeze_to_string(session):
+    out = freeze_to_string('http://test/simple-string', session)
+
+    assert out == "Mock!"
